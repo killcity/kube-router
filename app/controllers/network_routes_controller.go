0f5f2d182179dddf88ce9edcdd5e4f8eedb0e73c
BJ Badyk <bjbadyk@gmail.com>
2018-04-02 23:53:51 -0400

    Update network_routes_controller.go

--- app/controllers/network_routes_controller.go
+++ app/controllers/network_routes_controller.go
@@ -1131,7 +1131,7 @@ func (nrc *NetworkRoutingController) syncInternalPeers() {
 		n := &config.Neighbor{
 			Config: config.NeighborConfig{
 				NeighborAddress: nodeIP.String(),
-				PeerAs:          nrc.NodeAsnNumber,
+				PeerAs:          nrc.nodeAsnNumber,
 			},
 		}
 
